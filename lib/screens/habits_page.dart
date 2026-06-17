d739cd79a4118497f2eeedf6e0b388de6e24ca1f
Omuuletgit config --global user.name <rachelsim646@gmail.com>
2026-06-17 21:06:52 +0800

    work: save current progress on habits and home pages

--- lib/screens/habits_page.dart
+++ lib/screens/habits_page.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:crumb/services/habit_service.dart';
 import 'package:streak_calendar/streak_calendar.dart';
 import 'package:crumb/screens/profile_page.dart';
+import 'package:crumb/widgets/top_header.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 
 final HabitService habitService = HabitService();
 
@@ -12,63 +14,69 @@ class HabitsPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: const Color(0xFFEAE8D7),
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF8B6B4A),
-        foregroundColor: Colors.white,
-        title: const Text("Habits"),
-        centerTitle: true,
-      ),
-      body: StreamBuilder(
-        stream: habitService.getStreakStream(),
-        builder: (context, snapshot) {
-          if (!snapshot.hasData) {
-            return const Center(child: CircularProgressIndicator());
-          }
 
-          final doc = snapshot.data!.data();
+      body: Column(
+        children: [
+          TopHeader(
+            title: "Habits",
+            uid: FirebaseAuth.instance.currentUser!.uid,
+          ),
 
-          List raw = doc?['dates'] ?? [];
+          Expanded(
+            child: StreamBuilder(
+              stream: habitService.getStreakStream(),
+              builder: (context, snapshot) {
+                if (!snapshot.hasData) {
+                  return const Center(child: CircularProgressIndicator());
+                }
 
-          final datesForStreaks = raw.map((e) => DateTime.parse(e)).toList();
+                final doc = snapshot.data!.data();
+                List raw = doc?['dates'] ?? [];
+                final datesForStreaks = raw
+                    .map((e) => DateTime.parse(e))
+                    .toList();
 
-          return CleanCalendar(
-            enableDenseViewForDates: true,
-            enableDenseSplashForDates: true,
-            datesForStreaks: datesForStreaks,
-            currentDateProperties: DatesProperties(
-              datesDecoration: DatesDecoration(
-                datesBorderRadius: 1000,
-                datesBackgroundColor: Colors.transparent,
-                datesBorderColor: Colors.green,
-                datesTextColor: Colors.black,
-              ),
+                return CleanCalendar(
+                  enableDenseViewForDates: true,
+                  enableDenseSplashForDates: true,
+                  datesForStreaks: datesForStreaks,
+                  currentDateProperties: DatesProperties(
+                    datesDecoration: DatesDecoration(
+                      datesBorderRadius: 1000,
+                      datesBackgroundColor: Colors.transparent,
+                      datesBorderColor: Colors.green,
+                      datesTextColor: Colors.black,
+                    ),
+                  ),
+                  generalDatesProperties: DatesProperties(
+                    datesDecoration: DatesDecoration(
+                      datesBorderRadius: 1000,
+                      datesBackgroundColor: Colors.transparent,
+                      datesBorderColor: Colors.transparent,
+                      datesTextColor: Colors.black,
+                    ),
+                  ),
+                  streakDatesProperties: DatesProperties(
+                    datesDecoration: DatesDecoration(
+                      datesBorderRadius: 1000,
+                      datesBackgroundColor: Colors.green,
+                      datesBorderColor: Colors.green,
+                      datesTextColor: Colors.white,
+                    ),
+                  ),
+                  leadingTrailingDatesProperties: DatesProperties(
+                    datesDecoration: DatesDecoration(
+                      datesBorderRadius: 1000,
+                      datesBackgroundColor: Colors.transparent,
+                      datesBorderColor: Colors.transparent,
+                      datesTextColor: Colors.grey,
+                    ),
+                  ),
+                );
+              },
             ),
-            generalDatesProperties: DatesProperties(
-              datesDecoration: DatesDecoration(
-                datesBorderRadius: 1000,
-                datesBackgroundColor: Colors.transparent,
-                datesBorderColor: Colors.transparent,
-                datesTextColor: Colors.black,
-              ),
-            ),
-            streakDatesProperties: DatesProperties(
-              datesDecoration: DatesDecoration(
-                datesBorderRadius: 1000,
-                datesBackgroundColor: Colors.green,
-                datesBorderColor: Colors.green,
-                datesTextColor: Colors.white,
-              ),
-            ),
-            leadingTrailingDatesProperties: DatesProperties(
-              datesDecoration: DatesDecoration(
-                datesBorderRadius: 1000,
-                datesBackgroundColor: Colors.transparent,
-                datesBorderColor: Colors.transparent,
-                datesTextColor: Colors.grey,
-              ),
-            ),
-          );
-        },
+          ),
+        ],
       ),
 
       bottomNavigationBar: BottomNavigationBar(
--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -6,6 +6,7 @@ import 'package:crumb/screens/habits_page.dart';
 import 'package:dashed_progress_bar/dashed_progress_bar.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:crumb/widgets/top_header.dart';
 
 class HomePage extends StatefulWidget {
   HomePage({super.key});
@@ -54,7 +55,9 @@ class _HomePageState extends State<HomePage> {
                   TextField(
                     controller: durationController,
                     keyboardType: TextInputType.number,
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
                     decoration: const InputDecoration(
                       labelText: "Duration",
                       suffixText: "min",
@@ -133,28 +136,25 @@ class _HomePageState extends State<HomePage> {
           builder: (context, setDialogState) {
             return AlertDialog(
               title: const Text("Edit Habit"),
-
               content: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-
                   TextField(
                     controller: habitController,
                     decoration: const InputDecoration(labelText: "Habit"),
                   ),
-
                   const SizedBox(height: 10),
-
                   TextField(
                     controller: durationController,
                     keyboardType: TextInputType.number,
-                    inputFormatters: [FilteringTextInputFormatter.digitsOnly],
+                    inputFormatters: [
+                      FilteringTextInputFormatter.digitsOnly
+                    ],
                     decoration: const InputDecoration(
                       labelText: "Duration",
                       suffixText: "min",
                     ),
                   ),
-
                   if (errorMessage.isNotEmpty)
                     Padding(
                       padding: const EdgeInsets.only(top: 8),
@@ -165,13 +165,11 @@ class _HomePageState extends State<HomePage> {
                     ),
                 ],
               ),
-
               actions: [
                 TextButton(
                   onPressed: () => Navigator.pop(context),
                   child: const Text("Cancel"),
                 ),
-
                 ElevatedButton(
                   onPressed: () {
                     habitService.updateHabit(
@@ -228,196 +226,209 @@ class _HomePageState extends State<HomePage> {
 
     return Scaffold(
       backgroundColor: const Color(0xFFEAE8D7),
-
-      appBar: AppBar(
-        backgroundColor: const Color(0xFF8B6B4A),
-        title: const Text("Crumb"),
-        centerTitle: true,
-      ),
-
       body: Stack(
         children: [
-          Column(
-            children: [
-              StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                stream: FirebaseFirestore.instance
-                    .collection('users')
-                    .doc(uid)
-                    .snapshots(),
-                builder: (context, userSnap) {
-                  final goalDays = userSnap.data?.data()?['goalDays'] ?? 7;
+          SingleChildScrollView(
+            child: Column(
+              children: [
+                TopHeader(
+  title: "Home",
+  uid: uid,
+),
+                const SizedBox(height: 20),
+                StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
+                  stream: FirebaseFirestore.instance
+                      .collection('users')
+                      .doc(uid)
+                      .snapshots(),
+                  builder: (context, userSnap) {
+                    final goalDays = userSnap.data?.data()?['goalDays'] ?? 7;
 
-                  return StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-                    stream: FirebaseFirestore.instance
-                        .collection('users')
-                        .doc(uid)
-                        .collection('streaks')
-                        .doc('main')
-                        .snapshots(),
-                    builder: (context, streakSnap) {
-                      final data = streakSnap.data?.data();
-                      final List dates = data?['dates'] ?? [];
-                      final streakDays = dates.length;
+                    return StreamBuilder<
+                        DocumentSnapshot<Map<String, dynamic>>>(
+                      stream: FirebaseFirestore.instance
+                          .collection('users')
+                          .doc(uid)
+                          .collection('streaks')
+                          .doc('main')
+                          .snapshots(),
+                      builder: (context, streakSnap) {
+                        final data = streakSnap.data?.data();
+                        final List dates = data?['dates'] ?? [];
+                        final streakDays = dates.length;
 
-                      return Padding(
-                        padding: const EdgeInsets.only(top: 40),
-                        child: SizedBox(
-                          height: 220,
-                          child: DashedCircularProgressBar.aspectRatio(
-                            aspectRatio: 1,
-                            progress: streakDays.toDouble(),
-                            maxProgress: goalDays.toDouble(),
-                            foregroundColor: const Color(0xFF8B6B4A),
-                            backgroundColor: const Color(0xFFE0D6C8),
-                            foregroundStrokeWidth: 10,
-                            backgroundStrokeWidth: 10,
-                            animation: true,
-                            child: Center(
-                              child: Text(
-                                "$streakDays / $goalDays",
-                                style: const TextStyle(
-                                  fontSize: 22,
-                                  fontWeight: FontWeight.bold,
-                                  color: Color(0xFF8B6B4A),
+                        return Padding(
+                          padding: const EdgeInsets.only(top: 40),
+                          child: SizedBox(
+                            height: 220,
+                            child: DashedCircularProgressBar.aspectRatio(
+                              aspectRatio: 1,
+                              progress: streakDays.toDouble(),
+                              maxProgress: goalDays.toDouble(),
+                              foregroundColor: const Color(0xFF8B6B4A),
+                              backgroundColor: const Color(0xFFE0D6C8),
+                              foregroundStrokeWidth: 10,
+                              backgroundStrokeWidth: 10,
+                              animation: true,
+                              child: Center(
+                                child: Text(
+                                  "$streakDays / $goalDays",
+                                  style: const TextStyle(
+                                    fontSize: 22,
+                                    fontWeight: FontWeight.bold,
+                                    color: Color(0xFF8B6B4A),
+                                  ),
                                 ),
                               ),
                             ),
-                          ),
-                        ),
-                      );
-                    },
-                  );
-                },
-              ),
-
-              const SizedBox(height: 10),
-
-              Expanded(
-                child: StreamBuilder(
-                  stream: habitService.habits.snapshots(),
-                  builder: (context, snapshot) {
-                    if (!snapshot.hasData) {
-                      return const Center(child: CircularProgressIndicator());
-                    }
-
-                    final habits = snapshot.data!.docs;
-
-                    return ListView.builder(
-                      itemCount: habits.length,
-                      itemBuilder: (context, index) {
-                        final habit = habits[index];
-                        final data = habit.data();
-
-                        final today = habitService.getTodayString();
-                        final List completedDates =
-                            data['completedDates'] ?? [];
-
-                        final isDoneToday = completedDates.contains(today);
-
-                        return Card(
-                          child: ListTile(
-                            title: Text(data['title']),
-                            subtitle: Text(data['duration']),
-
-                            leading: Checkbox(
-                              value: isDoneToday,
-                              onChanged: (value) async {
-                                if (value == true) {
-                                  await habitService.markHabitDoneToday(
-                                    habit.id,
-                                  );
-                                } else {
-                                  await habitService.unmarkHabitToday(habit.id);
-                                }
-                              },
-                            ),
-
-                           
-                            trailing: PopupMenuButton<String>(
-                              onSelected: (value) {
-                                if (value == 'edit') {
-                                  showEditHabitDialog(context, habit.id, data);
-                                }
-
-                                if (value == 'delete') {
-                                  confirmDeleteHabit(context, habit.id);
-                                }
-                              },
-                              itemBuilder: (context) => const [
-                                PopupMenuItem(
-                                  value: 'edit',
-                                  child: Text('Edit'),
-                                ),
-                                PopupMenuItem(
-                                  value: 'delete',
-                                  child: Text('Delete'),
-                                ),
-                              ],
-                            ),
-
-                            onTap: () =>
-                                showHabitOptions(context, habit.id, data),
                           ),
                         );
                       },
                     );
                   },
                 ),
-              ),
-            ],
-          ),
-
-          StreamBuilder<DocumentSnapshot<Map<String, dynamic>>>(
-            stream: FirebaseFirestore.instance
-                .collection('users')
-                .doc(uid)
-                .collection('streaks')
-                .doc('main')
-                .snapshots(),
-            builder: (context, streakSnap) {
-              final data = streakSnap.data?.data();
-              final List dates = data?['dates'] ?? [];
-              final streakDays = dates.length;
-
-              return Positioned(
-                top: 50,
-                right: 15,
-                child: Container(
-                  padding: const EdgeInsets.symmetric(
-                    horizontal: 10,
-                    vertical: 6,
-                  ),
-                  decoration: BoxDecoration(
-                    color: const Color(0xFF8B6B4A),
-                    borderRadius: BorderRadius.circular(20),
-                  ),
-                  child: Row(
-                    mainAxisSize: MainAxisSize.min,
-                    children: [
-                      const Text("🔥"),
-                      const SizedBox(width: 4),
-                      Text(
-                        "$streakDays",
-                        style: const TextStyle(
-                          color: Colors.white,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ],
+                const SizedBox(height: 50),
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 50),
+                  child: Text(
+                    "Nice!! Keep showing up, your future self is watching.",
+                    textAlign: TextAlign.center,
+                    style: const TextStyle(
+                      color: Color(0xFF3E220F),
+                      fontSize: 20,
+                      fontWeight: FontWeight.w600,
+                    ),
                   ),
                 ),
-              );
-            },
+                const SizedBox(height: 10),
+                AnimatedSize(
+                  duration: const Duration(milliseconds: 300),
+                  curve: Curves.easeInOut,
+                  child: Container(
+                    margin: const EdgeInsets.all(12),
+                    padding: const EdgeInsets.all(12),
+                    decoration: BoxDecoration(
+                      color: const Color(0xFF78583E),
+                      borderRadius: BorderRadius.circular(16),
+                    ),
+                    child: Column(
+                      mainAxisSize: MainAxisSize.min,
+                      children: [
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            const Text(
+                              "Today's Habit",
+                              style: TextStyle(
+                                color: Colors.white,
+                                fontSize: 18,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            SizedBox(
+                              height: 35,
+                              child: ElevatedButton(
+                                style: ElevatedButton.styleFrom(
+                                  backgroundColor: const Color(0xFFEAE8D7),
+                                  foregroundColor: const Color(0xFF78583E),
+                                  padding: const EdgeInsets.symmetric(
+                                    horizontal: 20,
+                                  ),
+                                  shape: RoundedRectangleBorder(
+                                    borderRadius: BorderRadius.circular(20),
+                                  ),
+                                ),
+                                onPressed: () => showAddHabitDialog(context),
+                                child: const Text("+ Add"),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: 10),
+                        StreamBuilder(
+                          stream: habitService.habits.snapshots(),
+                          builder: (context, snapshot) {
+                            if (!snapshot.hasData) {
+                              return const Center(
+                                child: CircularProgressIndicator(
+                                  color: Colors.white,
+                                ),
+                              );
+                            }
+
+                            final habits = snapshot.data!.docs;
+
+                            return ListView.builder(
+                              shrinkWrap: true,
+                              physics: const NeverScrollableScrollPhysics(),
+                              itemCount: habits.length,
+                              itemBuilder: (context, index) {
+                                final habit = habits[index];
+                                final data = habit.data();
+
+                                final today = habitService.getTodayString();
+                                final List completedDates =
+                                    data['completedDates'] ?? [];
+
+                                final isDoneToday =
+                                    completedDates.contains(today);
+
+                                return Card(
+                                  color: const Color(0xFFEAE8D7),
+                                  child: ListTile(
+                                    title: Text(data['title']),
+                                    subtitle: Text(data['duration']),
+                                    leading: Checkbox(
+                                      value: isDoneToday,
+                                      activeColor: const Color(0xFF78583E),
+                                      onChanged: (value) async {
+                                        if (value == true) {
+                                          await habitService
+                                              .markHabitDoneToday(habit.id);
+                                        } else {
+                                          await habitService
+                                              .unmarkHabitToday(habit.id);
+                                        }
+                                      },
+                                    ),
+                                    trailing: PopupMenuButton<String>(
+                                      onSelected: (value) {
+                                        if (value == 'edit') {
+                                          showEditHabitDialog(
+                                              context, habit.id, data);
+                                        }
+                                        if (value == 'delete') {
+                                          confirmDeleteHabit(context, habit.id);
+                                        }
+                                      },
+                                      itemBuilder: (context) => const [
+                                        PopupMenuItem(
+                                          value: 'edit',
+                                          child: Text('Edit'),
+                                        ),
+                                        PopupMenuItem(
+                                          value: 'delete',
+                                          child: Text('Delete'),
+                                        ),
+                                      ],
+                                    ),
+                                    onTap: () => showHabitOptions(
+                                        context, habit.id, data),
+                                  ),
+                                );
+                              },
+                            );
+                          },
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
           ),
         ],
       ),
-
-      floatingActionButton: FloatingActionButton(
-        backgroundColor: const Color(0xFF8B6B4A),
-        onPressed: () => showAddHabitDialog(context),
-        child: const Icon(Icons.add),
-      ),
-
       bottomNavigationBar: BottomNavigationBar(
         backgroundColor: const Color(0xFF8B6B4A),
         selectedItemColor: Colors.white,
